79eebbc944833c978a4109211dbf4da277f2bc97
vinisg61-coder <vinisg61@gmail.com>
2026-06-28 12:51:36 -0300

    Update device configuration for Galaxy A52s 5G
    Removed copyright information and modified firmware source.

--- target/a52sxq/config.sh
+++ target/a52sxq/config.sh
@@ -1,24 +1,7 @@
-#
-# Copyright (C) 2025 Salvo Giangreco
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-
 # Device configuration file for Galaxy A52s 5G (a52sxq)
 TARGET_NAME="Galaxy A52s 5G"
 TARGET_CODENAME="a52sxq"
-SOURCE_FIRMWARE="SM-S911B/EUX/352404911234563"
+SOURCE_FIRMWARE="SM-S711B/EUX/352404911234563"
 TARGET_FIRMWARE="SM-A528B/BTU/352599501234566"
 TARGET_EXTRA_FIRMWARES=()
 TARGET_API_LEVEL=34
@@ -29,8 +12,6 @@ TARGET_OS_FILE_SYSTEM="erofs"
 TARGET_SUPER_PARTITION_SIZE=10643046400
 TARGET_SUPER_GROUP_SIZE=10638852096
 TARGET_HAS_SYSTEM_EXT=false
-
-# SEC Product Feature
 TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
 TARGET_AUDIO_SUPPORT_DUAL_SPEAKER=true
 TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
@@ -54,3 +35,6 @@ TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="0"
 TARGET_MULTI_MIC_MANAGER_VERSION="07010"
 TARGET_SSRM_CONFIG_NAME="siop_a52sxq_sm7325"
 TARGET_SUPPORT_CUTOUT_PROTECTION=true
+export DEBLOAT=1
+export AGGRESSIVE_DEBLOAT=1
+export SKIP_DEKNOX=0
